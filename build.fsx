d50f5ad350f21b1820683a5592d3005d2f3a8846
Nathan Evans <nathan@byteflag.com>
2015-05-04 02:41:19 +0100

    Second commit.

--- build.fsx
+++ build.fsx
@@ -1,9 +1,5 @@
 open System
 open System.IO
-//open System.Text.RegularExpressions
-
-//let bodyContentRegex = new Regex("\$BODYCONTENT\$", RegexOptions.Multiline)
-//let navBarContentRegex = new Regex("\$NAVBARCONTENT\$", RegexOptions.Multiline)
 
 type Page = Page of FileName : string * DisplayName : string * Pages : Page list
 
@@ -20,24 +16,26 @@ let generateNavBarContent currentPage =
     |> Seq.map (
         function
         | Page (fileName, displayName, _) as page when currentPage = page ->
-            "<li class=\"active\"><a href=\"build_" + fileName + "\">" + displayName + """</a></li>"""
+            "<li class=\"active\"><a href=\"" + fileName + "\">" + displayName + """</a></li>"""
         | Page (fileName, displayName, _) ->
-            "<li><a href=\"build_" + fileName + "\">" + displayName + """</a></li>""")
+            "<li><a href=\"" + fileName + "\">" + displayName + """</a></li>""")
     |> String.concat ""
 
-let rebasePath path = Path.Combine("/Users/nbevans/Downloads/byteflag-web", path)
+let baseFolderPath = __SOURCE_DIRECTORY__
+let contentFolderPath = Path.Combine(baseFolderPath, "content")
+let contentFileName path = Path.Combine(contentFolderPath, path)
+let outputFileName path = Path.Combine(baseFolderPath, path)
 
 let read page =
-    match page with Page (fileName, _, _) when fileName <> "" -> File.ReadAllText(rebasePath fileName) | _ -> ""
-
-printfn "%A" pages
+    match page with Page (fileName, _, _) when fileName <> "" -> File.ReadAllText(contentFileName fileName) | _ -> ""
 
 pages
 |> Seq.iter (
     function
     | Page (fileName, _, _) as page ->
-        let template = File.ReadAllText("/Users/nbevans/Downloads/byteflag-web/template.html")
+        let template = File.ReadAllText(contentFileName "template.html")
         let pageContent = template
         let pageContent = pageContent.Replace("$NAVBARCONTENT$", generateNavBarContent page)
         let pageContent = pageContent.Replace("$BODYCONTENT$", match read page with v when v <> "" -> v | _ -> "Nothing to display on this page yet. Come back later!")
-        File.WriteAllText("build_" + fileName, pageContent))
+        File.WriteAllText(outputFileName fileName, pageContent)
+        printfn "%A" page)
