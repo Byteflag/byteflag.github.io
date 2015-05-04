5ef5a1d9502742fe12af62911cf4c6efe5c321cd
Nathan Evans <nathan@byteflag.com>
2015-05-04 08:20:15 +0100

    More changes.

--- build.fsx
+++ build.fsx
@@ -5,17 +5,18 @@ type Page = Page of FileName : string * DisplayName : string * Pages : Page list
 
 let pages = [
     Page ("index.html", "Home", [])
-    Page ("company.html", "Company", [])
-    Page ("panconnect.html", "PanConnect", [])
+    Page ("panconnect.html", "PanConnect", [  Page ("panconnect_mobile.html", "PanConnect Mobile", [])
+                                              Page ("panconnect_selfservice.html", "PanConnect Self-Service", []) ])
     Page ("consulting.html", "Consulting", [])
     Page ("hiring.html", "We're hiring", [])
+    Page ("company.html", "Company", [])
 ]
 
-let generateNavBarContent currentPage =
+let generateNavBarContent topLevelPage =
     pages
     |> Seq.map (
         function
-        | Page (fileName, displayName, _) as page when currentPage = page ->
+        | Page (fileName, displayName, _) as page when topLevelPage = page ->
             "<li class=\"active\"><a href=\"" + fileName + "\">" + displayName + """</a></li>"""
         | Page (fileName, displayName, _) ->
             "<li><a href=\"" + fileName + "\">" + displayName + """</a></li>""")
@@ -30,16 +31,22 @@ let read page =
     match page with Page (fileName, _, _) when fileName <> "" -> File.ReadAllText(contentFileName fileName) | _ -> ""
 
 while true do
-    pages
-    |> Seq.iter (
+    let rec processPage topLevelPage =
         function
-        | Page (fileName, _, _) as page ->
+        | Page (fileName, _, pages) as page ->
             let template = File.ReadAllText(contentFileName "template.html")
             let pageContent = template
-            let pageContent = pageContent.Replace("$NAVBARCONTENT$", generateNavBarContent page)
+            let pageContent = pageContent.Replace("$NAVBARCONTENT$", generateNavBarContent topLevelPage)
             let pageContent = pageContent.Replace("$BODYCONTENT$", match read page with v when v <> "" -> v | _ -> "Nothing to display on this page yet. Come back later!")
             File.WriteAllText(outputFileName fileName, pageContent)
-            printfn "%A" page)
+
+            // now recurse into the nested pages
+            pages |> Seq.iter (processPage topLevelPage)
+
+            // trace
+            printfn "%A" page
+
+    pages |> Seq.iter (fun page -> processPage page page)
 
     System.Threading.Thread.Sleep 3000
     printfn "\r\nLooping...\r\n"
